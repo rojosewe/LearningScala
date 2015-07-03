bac4f43f7d10e8640cdbc829c8ad4bacb2b57b66
“Rodrigo <rweffer@sensefields.com>
2015-07-03 18:47:38 +0200

    Awesome. Understood AutoTypeDependentClosure.

--- src/JavaTesting/CommentFormatter.java
+++ src/JavaTesting/CommentFormatter.java
@@ -1,15 +1,11 @@
 package JavaTesting;
 
 /**
- * The function whileLoop takes two parameters cond and body. When the function
- * is applied, the actual parameters do not get evaluated. But whenever the
- * formal parameters are used in the body of whileLoop, the implicitly created
- * nullary functions will be evaluated instead. Thus, our method whileLoop
- * implements a Java-like while-loop with a recursive implementation scheme.
- * 
- * We can combine the use of infix/postfix operators with this mechanism to
- * create more complex statements (with a nice syntax).
- * 
+ * The loop function just accepts a body of a loop and returns an instance of
+ * class LoopUnlessCond (which encapsulates this body object). Note that the
+ * body didn't get evaluated yet. Class LoopUnlessCond has a method unless which
+ * we can use as a infix operator. This way, we achieve a quite natural syntax
+ * for our new loop: loop { < stats > } unless ( < cond > ).
  * 
  * @author Rodrigo Weffer
  *
--- src/Tutorials/AutoTypeDependentClosure.scala
+++ src/Tutorials/AutoTypeDependentClosure.scala
@@ -1,7 +1,14 @@
 package Tutorials
 
 /**
- * @author sensefields
+ * The loop function just accepts a body of a loop and returns an instance of
+ * class LoopUnlessCond (which encapsulates this body object). Note that the
+ * body didn't get evaluated yet. Class LoopUnlessCond has a method unless which
+ * we can use as a infix operator. This way, we achieve a quite natural syntax
+ * for our new loop: loop { < stats > } unless ( < cond > ).
+ * 
+ * @author Rodrigo Weffer
+ *
  */
 object AutoTypeDependentClosure {
 
